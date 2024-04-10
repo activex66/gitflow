bd7980241886b76e0ff150b30a7726f5186e1faa
activex66 <xanxik@yandex.ru>
2024-04-10 23:01:45 +0300

    Добавил функцию замены первой буквы слова, добавил docstring

--- str_func.py
+++ str_func.py
@@ -1,7 +1,15 @@
 def capital_letters() -> str:
+    """"Изменяет строку на заглавные,
+     поступившей на вход функции"""
     user_word = input()
     word_upper = user_word.upper()
     return word_upper
 
 
 print(capital_letters())
+
+
+def words_title(word) -> str:
+    """Изменяет первые буквы каждого слова в строке,
+     поступившей на вход функции"""
+    return word.title()
